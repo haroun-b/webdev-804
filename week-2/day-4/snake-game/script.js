6f14951b673901ffef9a67423293be0178b03b46
Haroun B <haroun@holyduck.dev>
2024-04-17 20:48:50 +0200

    snake game starter

---
+++ week-2/day-4/snake-game/script.js
@@ -0,0 +1,14 @@
+const game = {
+  gridEl: document.querySelector("#game-grid"),
+  gridSize: 20,
+  cells: [],
+  foodPosition: null,
+  snake: {
+    direction: "right",
+    positions: []
+  },
+  buildCells() {},
+  start() {},
+  refresh() {},
+  end(result) {}
+};
