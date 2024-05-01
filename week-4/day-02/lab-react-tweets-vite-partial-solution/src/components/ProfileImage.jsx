9a5e23d8a8d7c2634479b24072acc73076680c1c
Haroun B <haroun@holyduck.dev>
2024-05-01 09:53:15 +0200

    partial solution to tweets lab

---
+++ week-4/day-02/lab-react-tweets-vite-partial-solution/src/components/ProfileImage.jsx
@@ -0,0 +1,11 @@
+function ProfileImage({ imageSrc }) {
+  return (
+    <img
+      src={imageSrc}
+      className="profile"
+      alt="profile"
+    />
+  );
+}
+
+export default ProfileImage;
---
+++ week-4/day-02/lab-react-tweets-vite-partial-solution/src/components/Tweet.jsx
@@ -0,0 +1,49 @@
+import ProfileImage from "./ProfileImage";
+
+function Tweet({ tweet }) {
+  return (
+    <div className="tweet">
+      <ProfileImage imageSrc={tweet.user.image} />
+
+      <div className="body">
+        <div className="top">
+          <span className="user">
+            <span className="name">{tweet.user.name}</span>
+            <span className="handle">@{tweet.user.handle}</span>
+          </span>
+
+          <span className="timestamp">{tweet.timestamp}</span>
+        </div>
+
+        <p className="message">
+          On December 7th, we will be hosting a #webinar that will introduce you
+          to #SQL! Are you ready? 🚀
+        </p>
+
+        <div className="actions">
+          {/* Font Awesome icons */}
+          <i
+            className="far fa-comment"
+            data-testid="comment-icon"
+          ></i>
+          <i
+            className="fas fa-retweet"
+            data-testid="retweet-icon"
+          ></i>
+          <i
+            className="far fa-heart"
+            data-testid="heart-icon"
+          ></i>
+          <i
+            className="fas fa-share"
+            data-testid="share-icon"
+          ></i>
+        </div>
+      </div>
+
+      <i className="fas fa-ellipsis-h"></i>
+    </div>
+  );
+}
+
+export default Tweet;
