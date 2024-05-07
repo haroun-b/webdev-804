2bba4e04dd732fb3aa10def58002d49f683b96a2
Haroun B <haroun@holyduck.dev>
2024-05-07 10:23:03 +0200

    counter with useEffect cleanup

--- week-5/day-02/react-counter/src/App.jsx
+++ week-5/day-02/react-counter/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Counter from "./components/Counter";
 
 /** Component life cycle
  * 1. Mount
@@ -8,29 +9,17 @@ import "./App.css";
  */
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [dependent, setDependant] = useState(false);
+  const [counterOn, setCounterOn] = useState(false);
 
-  useEffect(() => {
-    setInterval(() => {
-      setCount((currentCount) => currentCount + 1);
-    }, 1000);
-  }, [dependent]);
-
-  // setTimeout(() => {
-  //   // console.log(count);
-  //   setCount((currentCount) => {
-  //     console.log(currentCount);
-  //     return currentCount;
-  //   });
-  // }, 3000);
+  // useEffect(() => {
+  //   console.log("counterOn was switched");
+  // }, [counterOn]);
 
   return (
     <>
-      <h1>{count}</h1>
-      <button onClick={() => setCount((count) => count + 1)}>Increment</button>
-      <button onClick={() => setDependant((dependant) => !dependant)}>
-        Flip
+      {counterOn && <Counter />}
+      <button onClick={() => setCounterOn((_switch) => !_switch)}>
+        {counterOn ? "Hide Counter" : "Show Counter"}
       </button>
     </>
   );
---
+++ week-5/day-02/react-counter/src/components/Counter.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Counter has mounted");
+
+    const intervalId = setInterval(() => {
+      console.log("incrementing counter with interval...");
+      setCount((currentCount) => currentCount + 1);
+    }, 1000);
+
+    return () => {
+      console.log("Counter has unmounted");
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return <h1>{count}</h1>;
+}
+
+export default Counter;
