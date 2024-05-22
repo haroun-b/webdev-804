d6861d9a7108d3cdea5ea4df6d72adda82a69e9a
Haroun B <haroun@holyduck.dev>
2024-05-22 14:46:31 +0200

    mongoose example

--- week-7/day-03/mongoose/index.js
+++ week-7/day-03/mongoose/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+
 const Human = require("./models/Human.model");
-const humans = [{ id: 1, name: "Ulysse", phone: "0637349855" }];
+const Pet = require("./models/Pet.model");
+
+const serverErrorMsg = { message: "Internal Server Error" };
 
 const port = 3000;
 const app = express();
@@ -12,10 +15,6 @@ app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
-});
-
-app.get("/humans", (req, res) => {
-  res.json(humans);
 });
 
 app.post("/humans", async (req, res) => {
@@ -29,9 +28,156 @@ app.post("/humans", async (req, res) => {
     if (error.message.includes("validation")) {
       res.status(400).json({ message: "Invalid input" });
     } else {
-      res.status(500).json({ message: "Internal Server Error" });
+      res.status(500).json(serverErrorMsg);
     }
   }
+});
+
+app.get("/humans", async (req, res) => {
+  try {
+    const allHumans = await Human.find();
+    res.json(allHumans);
+  } catch (error) {
+    res.status(500).json(serverErrorMsg);
+  }
+});
+
+app.get("/humans/:humanId", async (req, res) => {
+  const { humanId } = req.params;
+  const notFoundMsg = { message: `No such human with id: ${humanId}` };
+
+  if (!mongoose.isValidObjectId(humanId)) {
+    res.status(404).json(notFoundMsg);
+    return;
+  }
+
+  try {
+    // const human = await Human.findOne({ _id: humanId });
+    const human = await Human.findById(humanId);
+
+    if (!human) {
+      res.status(404).json(notFoundMsg);
+      return;
+    }
+    res.json(human);
+  } catch (error) {
+    res.status(500).json(serverErrorMsg);
+  }
+});
+
+app.get("/humans/:humanId/pets", async (req, res) => {
+  const { humanId } = req.params;
+
+  const notFoundMsg = { message: `No such human with id: ${humanId}` };
+  if (!mongoose.isValidObjectId(humanId)) {
+    res.status(404).json(notFoundMsg);
+    return;
+  }
+
+  try {
+    const humanPets = await Pet.find({ owner: humanId }, { owner: 0 });
+    res.json(humanPets);
+  } catch (error) {
+    res.status(500).json(serverErrorMsg);
+  }
+});
+
+app.post("/humans/:humanId/pets", async (req, res) => {
+  const { humanId } = req.params;
+
+  const notFoundMsg = { message: `No such human with id: ${humanId}` };
+  if (!mongoose.isValidObjectId(humanId)) {
+    res.status(404).json(notFoundMsg);
+    return;
+  }
+
+  try {
+    const human = await Human.findById(humanId);
+
+    if (!human) {
+      res.status(400).json(notFoundMsg);
+      return;
+    }
+
+    const newPet = await Pet.create({ ...req.body, owner: humanId });
+    res.json(newPet);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "Invalid Input" });
+  }
+});
+
+app.get("/pets", async (req, res) => {
+  try {
+    const allPets = await Pet.find();
+    res.json(allPets);
+  } catch (error) {
+    res.status(500).json(serverErrorMsg);
+  }
+});
+
+app.put("/humans/:humanId", async (req, res) => {
+  const { humanId } = req.params;
+  const { name, phone } = req.body;
+  const notFoundMsg = { message: `No such human with id: ${humanId}` };
+
+  if (!mongoose.isValidObjectId(humanId)) {
+    res.status(404).json(notFoundMsg);
+    return;
+  }
+
+  try {
+    const updatedHuman = await Human.findByIdAndUpdate(
+      humanId,
+      {
+        name,
+        phone
+      },
+      { new: true } // to get the updated document instead of the old
+    );
+
+    res.json(updatedHuman);
+  } catch (error) {
+    res.status(400).json({ message: "Invalid Input" });
+  }
+});
+
+app.get("/pets/:petId", async (req, res) => {
+  const { petId } = req.params;
+
+  const notFoundMsg = { message: `No such pet with id: ${petId}` };
+  if (!mongoose.isValidObjectId(petId)) {
+    res.status(404).json(notFoundMsg);
+    return;
+  }
+  try {
+    const pet = await Pet.findById(petId).populate("owner");
+    if (!pet) {
+      res.status(404).json(notFoundMsg);
+      return;
+    }
+
+    res.json(pet);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(serverErrorMsg);
+  }
+});
+
+app.delete("/humans/:humanId", async (req, res) => {
+  const { humanId } = req.params;
+
+  if (!mongoose.isValidObjectId(humanId)) {
+    res.status(404).json({ message: `No such human with id: ${humanId}` });
+    return;
+  }
+
+  try {
+    await Human.findByIdAndDelete(humanId);
+    await Pet.deleteMany({ owner: humanId });
+  } catch (_) {}
+
+  res.sendStatus(204);
 });
 
 async function connectDB() {
@@ -42,7 +188,7 @@ async function connectDB() {
 
     console.log(`Connected to DB: ${db.connection.name}`);
   } catch (error) {
-    console.error(error);
+    console.log(`Could not connect to DB: ${error}`);
   }
 }
 connectDB();
--- week-7/day-03/mongoose/models/Pet.model.js
+++ week-7/day-03/mongoose/models/Pet.model.js
@@ -3,12 +3,12 @@ const { Schema, model } = require("mongoose");
 const petSchema = new Schema({
   name: { type: String, required: true, unique: true },
   description: { type: String, maxLength: 300 },
-  kind: { type: String, enum: ["cat", "dog"] },
+  kind: { type: String, enum: ["cat", "dog"], required: true },
   dateOfBirth: { type: Date, default: Date.now() },
-  weight: { type: Number, min: 0, max: 15 },
+  weight: { type: Number, min: 0, max: 15, required: true },
   isLame: { type: Boolean, default: false },
   favFoods: [String],
-  ownerId: {
+  owner: {
     type: Schema.Types.ObjectId,
     ref: "Human"
   }
